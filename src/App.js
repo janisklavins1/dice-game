8f1df90bd419910e2f14399351020e92b489c858
Kristaps Briks <kristaps.briks@inbox.lv>
2022-09-05 16:54:16 +0300

    Implement click dice functionality

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 import './App.css';
+import { useState } from 'react';
 import Dice from './components/Dice';
 
 function App() {
+
+  const [clicked, setClicked] = useState(false);
+
+  function clickItem() {
+      return setClicked(prev => !prev)
+  }
+
+  function rollDice(){
+    return null
+  }
+
   return (
     <div className="app">
       <main>
         <h1 className='heading'>Tenzies</h1>
         <p className='paragraph'>Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
-        <Dice />
-        <button>Roll</button>
+        <Dice clicked={clicked} clickItem={clickItem}/>
+        <button onClick={rollDice}>Roll</button>
       </main>
     </div>
   );
--- src/components/Dice.js
+++ src/components/Dice.js
@@ -1,13 +1,14 @@
 import React from 'react'
 
-const Dice = () => {
-  return (
-    <div className='dice-container'>
-        <div className='dice-box'>
-            <p className='dice-number'>1</p>
+const Dice = (props) => {
+
+    return (
+        <div className='dice-container'>
+            <div className={`dice-box ${ props.clicked ? 'clicked' : ''}`} onClick={props.clickItem}>
+                <p className='dice-number'>1</p>
+            </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Dice
