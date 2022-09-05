2749f9326cf691507ceb68fbcf9c310c3e9588b3
Kristaps Briks <kristaps.briks@inbox.lv>
2022-09-05 15:59:07 +0300

    Initial styling + layout

--- src/App.js
+++ src/App.js
@@ -2,8 +2,13 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      Hello
+    <div className="app">
+      <main>
+        <h1 className='heading'>Tenzies</h1>
+        <p className='paragraph'>Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
+        <p>Grid</p>
+        <button>Roll</button>
+      </main>
     </div>
   );
 }
